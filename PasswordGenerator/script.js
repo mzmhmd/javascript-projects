21d121c5004b980d4e554dd71e70d58d986f7f01
mzmhmd <mzmuhammad106@gmail.com>
2025-03-31 02:50:32 +0530

    feat: here is the 5th

---
+++ PasswordGenerator/script.js
@@ -0,0 +1,40 @@
+
+function generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+-=";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if(passwordLength<=0){
+        return `(password length must be atleast one)`;
+    }
+    if(allowedChars.length === 0){
+        return `(At least one set of characters needs to be selected)`;
+    }
+
+    for(let i=0; i<passwordLength; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+    return password;
+}
+
+const passwordLength = 8;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+
+console.log(`Generated Password: ${password}`);
